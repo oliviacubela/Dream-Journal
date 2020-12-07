be544ad5121a621dbab3f3cb4422e3653448bc2c
Olivia Cubela <oliviacubela@gmail.com>
2020-12-07 15:01:13 -0500

    resources call

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :dreams
+  resources :users
+  resources :themes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
