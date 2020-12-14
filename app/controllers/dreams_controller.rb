5b20c7dde7f3152dcc2374619f3af3ad91be887f
Olivia Cubela <oliviacubela@gmail.com>
2020-12-14 12:40:48 -0500

    added strong params and new/show/create methods

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,10 +1,26 @@
 class DreamsController < ApplicationController
 
+  def show
+    @dream = Dream.find(params[:id])
+  end
+
   def index 
     @dreams = Dream.all 
   end
 
-  def show 
-    
+  def new 
+    @dream = Dream.new 
   end
+
+  def create 
+    dream = Dream.create(dream_params) 
+    redirect_to dream_path 
+  end
+
+  private 
+
+  def dream_params
+    params.require(:dream).permit(:title, :content)
+  end
+
 end
