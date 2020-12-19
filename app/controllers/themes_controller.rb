b1a2c446bdf93d2f4fcd16913a5db3c9d3894d73
Olivia Cubela <oliviacubela@gmail.com>
2020-12-19 11:12:45 -0500

    added new, index, create, and show routes

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,2 +1,31 @@
 class ThemesController < ApplicationController
+
+  def new 
+    @theme = Theme.new
+  end
+
+  def create 
+    @theme = Theme.new(theme_params)
+
+    if @theme.save
+      redirect_to @theme
+    else
+      render :new
+    end
+  end
+
+  def index 
+    @themes = Theme.all
+  end
+
+
+  def show
+    @theme = Theme.find(params[:id])
+  end 
+
+  private
+
+  def theme_params
+    params.require(:theme).permit(:name, :description)
+  end
 end
