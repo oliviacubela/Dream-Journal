49975ec86bb78787bb5bdf0c00af6ae65a499cce
Olivia Cubela <oliviacubela@gmail.com>
2020-12-19 11:15:29 -0500

    added setter & getter to dream model

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,4 +1,13 @@
 class Dream < ActiveRecord::Base
   belongs_to :user 
   belongs_to :theme
+
+  def theme_name
+    self.try(:theme).try(:name, :description)
+  end
+
+  def theme_name=(name)
+    theme = Theme.find_or_create_by(name: name, description: description)
+    self.theme = theme
+  end
 end
